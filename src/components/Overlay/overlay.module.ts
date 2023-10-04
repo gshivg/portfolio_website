0548572af3761d6f255a97097d61478bdf3957a6
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-10-04 10:52:47 +0530

    scroll fix

--- src/components/Overlay/overlay.module.ts
+++ src/components/Overlay/overlay.module.ts
@@ -7,11 +7,16 @@ import childrenNumberCalculator from "../../ts/sectionNumberCalculator";
 let scrollValue = window.scrollY;
 let windowHeight = window.innerHeight;
 
-const sectionCount = childrenNumberCalculator("section-wrapper");
+const sectionCount = childrenNumberCalculator("section-wrapper")! - 1;
+console.log("sectionCount", sectionCount);
 
 window.addEventListener("scroll", () => {
   scrollValue = window.scrollY;
-  let currentScroll = (scrollValue / windowHeight) * 100;
+
+  let currentScroll = (scrollValue / (windowHeight * sectionCount)) * 100;
+  if (currentScroll > 100) {
+    currentScroll = 100;
+  }
   document
     .getElementById("progress")!
     .setAttribute("style", `height: ${currentScroll}%`);
